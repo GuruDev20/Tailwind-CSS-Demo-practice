88e58e921e01d9f9038c34b9b9d539f576785dd2
Guru Dev <guru01803@gmail.com>
2023-12-09 21:24:15 +0530

    text decorations

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
-export default function App() {
+import React from 'react';
+function App() {
   return (
     <div>
-      <h1>Hello world!</h1>
-      <h1>Hello world!</h1>
+      <h1 className='text-3xl italic underline decoration-red-500 decoration-wavy decoration-1 underline-offset-4'>Title 1</h1>
+      <h1 className='text-2xl'>Title 2</h1>
+      <h1 className='text-xl'>Title 3</h1>
+      <p className='text-base'>A regular paragraph</p>
+      <p className='text-sm'>A description paragrap</p>
+      <p className="note text-xs">A Little note</p>
     </div>
   );
 }
+export default App;
