eb1e40bff9403a14a1c8305415c38463a53d3a1b
Guru Dev <guru01803@gmail.com>
2023-12-09 20:57:13 +0530

    handling custom spaces and screen sizes

--- src/App.js
+++ src/App.js
@@ -2,8 +2,8 @@ import React from "react";
 export default function App() {
   return (
     <div>
-      <h1 className="border-4 border-green-700">Hello world!</h1>
-      <h1 className="text-vuejs">Hello world!</h1>
+      <h1 className="mb-1">Hello world!</h1>
+      <h1>Hello world!</h1>
     </div>
   );
 }
