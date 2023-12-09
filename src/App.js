64ba22f6c1ad8c97a0647c65e0db4ff7142f6313
Guru Dev <guru01803@gmail.com>
2023-12-09 21:04:36 +0530

    using plugings to add styles

--- src/App.js
+++ src/App.js
@@ -1,8 +1,7 @@
-import React from "react";
 export default function App() {
   return (
     <div>
-      <h1 className="mb-1">Hello world!</h1>
+      <h1>Hello world!</h1>
       <h1>Hello world!</h1>
     </div>
   );
