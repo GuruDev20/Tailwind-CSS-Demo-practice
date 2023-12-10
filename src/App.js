038ed94848c7669f5a35d43143a21c6f8436d6cc
Guru Dev <guru01803@gmail.com>
2023-12-10 10:44:12 +0530

    MARGIN STYLES

--- src/App.js
+++ src/App.js
@@ -2,12 +2,13 @@ import React from 'react';
 function App() {
   return (
     <div>
-      <div className='bg-red-500 text-white px-4'>Hello</div><br/>
-      <div className='bg-red-500 text-white py-4'>Hello</div><br/>
-      <div className='bg-red-500 text-white pt-4'>Hello</div><br/>
-      <div className='bg-red-500 text-white pb-4'>Hello</div><br/>
-      <div className='bg-red-500 text-white pl-4'>Hello</div><br/>
-      <div className='bg-red-500 text-white pr-4'>Hello</div><br/>
+      <div className='bg-red-500 text-white m-4'>Hello</div><br/>
+      <div className='bg-red-500 text-white mx-4'>Hello</div><br/>
+      <div className='bg-red-500 text-white my-4'>Hello</div><br/>
+      <div className='bg-red-500 text-white mt-4'>Hello</div><br/>
+      <div className='bg-red-500 text-white mb-4'>Hello</div><br/>
+      <div className='bg-red-500 text-white ml-4'>Hello</div><br/>
+      <div className='bg-red-500 text-white mr-4'>Hello</div><br/>
     </div>
   );
 }
