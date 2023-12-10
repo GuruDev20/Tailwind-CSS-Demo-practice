55da3955467b8797e677c80a178a59708d14ec34
Guru Dev <guru01803@gmail.com>
2023-12-10 10:39:25 +0530

    padding

--- src/App.js
+++ src/App.js
@@ -2,12 +2,12 @@ import React from 'react';
 function App() {
   return (
     <div>
-      <h1 className='text-3xl italic underline decoration-red-500 decoration-wavy decoration-1 underline-offset-4'>Title 1</h1>
-      <h1 className='text-2xl'>Title 2</h1>
-      <h1 className='text-xl'>Title 3</h1>
-      <p className='text-base'>A regular paragraph</p>
-      <p className='text-sm'>A description paragrap</p>
-      <p className="note text-xs">A Little note</p>
+      <div className='bg-red-500 text-white px-4'>Hello</div><br/>
+      <div className='bg-red-500 text-white py-4'>Hello</div><br/>
+      <div className='bg-red-500 text-white pt-4'>Hello</div><br/>
+      <div className='bg-red-500 text-white pb-4'>Hello</div><br/>
+      <div className='bg-red-500 text-white pl-4'>Hello</div><br/>
+      <div className='bg-red-500 text-white pr-4'>Hello</div><br/>
     </div>
   );
 }
