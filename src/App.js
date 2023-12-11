9510edfedc0c55c65cd2de1e7be6c912f4d03f71
Guru Dev <guru01803@gmail.com>
2023-12-11 22:15:05 +0530

    grid & properties

--- src/App.js
+++ src/App.js
@@ -1,40 +1,31 @@
 import React from "react";
 function App() {
   return (
-    // <div className="parent">
-    //   <div>Header</div>
-    //   <div className="flex">
-    //     <div className="basis-1/4">Sidebar</div>
-    //     <div className="basis-2/4">Main Content</div>
-    //     <div className="basis-1/4">Another Sidebar</div>
-    //   </div>
-    // </div>
-    // <div className="parent">
-    //   <div>Header</div>
-    //   <div className="flex flex-col md:flex-row">
-    //     <div className="w-32 bg-sky-500">Sidebar</div>
-    //     <div className="w-full bg-red-500">Main content</div>
-    //   </div>
-    // </div>
-    // <div className="parent">
-    //   <div>Header</div>
-    //   <div className="flex flex-col md:flex-row">
-    //     <div className="flex flex-nowrap w-full">
-    //       <div className="w-full grow">01</div>
-    //       <div className="w-full grow">02</div>
-    //       <div className="w-full grow">03</div>
-    //     </div>
-    //   </div>
-    // </div>
-    <div className="parent">
-      <div>Header</div>
-      <div className="flex items-center justify-evenly">
-        <div>Sidebar</div>
-        <div>
-          <h1>Main content</h1>
-          <p>Loreum ipsum </p>
+    <div className="h-screen text-white parent bg-slate-900">
+      <div className="container mx-auto">
+        {/* <div className="grid lg:grid-cols-3 md:grid-cols-2 gap-4">
+          <div className="bg-sky-500 p-6 rounded-lg">1</div>
+          <div className="bg-sky-500 p-6 rounded-lg">2</div>
+          <div className="bg-sky-500 p-6 rounded-lg">3</div>
+          <div className="bg-red-500 p-6 rounded-lg col-span-2">4</div>
+          <div className="bg-sky-500 p-6 rounded-lg">5</div>
+          <div className="bg-sky-500 p-6 rounded-lg">6</div>
+          <div className="bg-green-500 p-6 rounded-lg col-span-2">7</div>
+        </div> */}
+        <div class="grid grid-flow-row-dense grid-cols-3 grid-rows-3 gap-4">
+          <div className="p-6 bg-sky-500 rounded-lg col-span-2">01</div>
+          <div className="p-6 bg-sky-500 rounded-lg col-span-2">02</div>
+          <div className="p-6 bg-sky-500 rounded-lg">03</div>
+          <div className="p-6 bg-sky-500 rounded-lg">04</div>
+          <div className="p-6 bg-sky-500 rounded-lg">05</div>
+          <div className="p-6 bg-sky-500 rounded-lg col-span-2">06</div>
+          <div className="p-6 bg-sky-500 rounded-lg">07</div>
+          <div className="p-6 bg-sky-500 rounded-lg">08</div>
+          <div className="p-6 bg-sky-500 rounded-lg">09</div>
+          <div className="p-6 bg-sky-500 rounded-lg col-span-2">10</div>
+          <div className="p-6 bg-sky-500 rounded-lg">11</div>
+          <div className="p-6 bg-sky-500 rounded-lg">12</div>
         </div>
-        <div>Another Sidebar</div>
       </div>
     </div>
   );
