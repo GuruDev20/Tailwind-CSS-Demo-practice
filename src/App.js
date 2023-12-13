4d7aa5785ef46afa57c8e964ff7d38f91bc4622a
Guru Dev <guru01803@gmail.com>
2023-12-13 21:27:44 +0530

    effects & filters

--- src/App.js
+++ src/App.js
@@ -1,9 +1,26 @@
 import React from "react";
+import tanjiro from './tanjiro.png';
 function App() {
   return (
-    <div className="h-screen text-white parent bg-slate-900">
-      {/* <header className="w-full border-b-4 py-4 border-green-500">Header</header> */}
-      <button className="ring-offset-2 ring-4">Button C</button>
+    <div className="h-screen p-4 parent">
+      <div className="inline-block p-4 text-black bg-white border rounded-lg shadow-md  grayscale hover:grayscale-0">
+        <img src={tanjiro} alt="" className="w-64 mg-4"/>
+        <h1 className="text-2xl">Hello</h1>
+        <p className="mb-4">This is my div</p>
+        <button className="px-3 py-2 rounded-lg cursor-pointer bg-cyan-500 shadow-md shadow-cyan-500/50 opacity-50 text-white">Say hello</button>
+      </div>
+      {/* <div className="inline-block p-4 text-black bg-white border rounded-lg shadow-md  brightness-50 hover:brightness-100">
+        <img src={tanjiro} alt="" className="w-64 mg-4"/>
+        <h1 className="text-2xl">Hello</h1>
+        <p className="mb-4">This is my div</p>
+        <button className="px-3 py-2 rounded-lg cursor-pointer bg-cyan-500 shadow-md shadow-cyan-500/50 opacity-50 text-white">Say hello</button>
+      </div> */}
+      {/* <div className="inline-block p-4 text-black bg-white border rounded-lg shadow-md blur hover:blur-none">
+        <img src={tanjiro} alt="" className="w-64 mg-4"/>
+        <h1 className="text-2xl">Hello</h1>
+        <p className="mb-4">This is my div</p>
+        <button className="px-3 py-2 rounded-lg cursor-pointer bg-cyan-500 shadow-md shadow-cyan-500/50 opacity-50 text-white">Say hello</button>
+      </div> */}
     </div>
   );
 }
