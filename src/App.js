236903ade8e9f9e69eb3a0a86d7ec7533a1105d7
Guru Dev <guru01803@gmail.com>
2023-12-15 22:58:19 +0530

    transition & animation

--- src/App.js
+++ src/App.js
@@ -1,26 +1,12 @@
 import React from "react";
-import tanjiro from './tanjiro.png';
 function App() {
   return (
     <div className="h-screen p-4 parent">
-      <div className="inline-block p-4 text-black bg-white border rounded-lg shadow-md  grayscale hover:grayscale-0">
-        <img src={tanjiro} alt="" className="w-64 mg-4"/>
-        <h1 className="text-2xl">Hello</h1>
-        <p className="mb-4">This is my div</p>
-        <button className="px-3 py-2 rounded-lg cursor-pointer bg-cyan-500 shadow-md shadow-cyan-500/50 opacity-50 text-white">Say hello</button>
-      </div>
-      {/* <div className="inline-block p-4 text-black bg-white border rounded-lg shadow-md  brightness-50 hover:brightness-100">
-        <img src={tanjiro} alt="" className="w-64 mg-4"/>
-        <h1 className="text-2xl">Hello</h1>
-        <p className="mb-4">This is my div</p>
-        <button className="px-3 py-2 rounded-lg cursor-pointer bg-cyan-500 shadow-md shadow-cyan-500/50 opacity-50 text-white">Say hello</button>
-      </div> */}
-      {/* <div className="inline-block p-4 text-black bg-white border rounded-lg shadow-md blur hover:blur-none">
-        <img src={tanjiro} alt="" className="w-64 mg-4"/>
-        <h1 className="text-2xl">Hello</h1>
-        <p className="mb-4">This is my div</p>
-        <button className="px-3 py-2 rounded-lg cursor-pointer bg-cyan-500 shadow-md shadow-cyan-500/50 opacity-50 text-white">Say hello</button>
-      </div> */}
+      <h1 className="text-2xl">Hello</h1>
+      <p className="mb-4"> This is my di</p>
+      <button className="px-3 py-2 text-white transition delay-1000 rounded-lg shadow-md cursoe-pointer hover:-translate-y-1 duration-1000 animate-bounce ease-in-out bg-cyan-500 shadow-cyan-500/50">
+        Say hello
+      </button>
     </div>
   );
 }
