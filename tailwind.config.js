ad016b774a95803bb2ae7142b64f010aec8decee
Guru Dev <guru01803@gmail.com>
2023-12-20 20:44:37 +0530

    updated

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,21 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  darkMode:'class',
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
-  theme: {
-    container: {
-      center: true,
-    },
-    extend: {
-      margin:{
-        1:'14px',
-        2:'16px',
-        3:'18px',
-        4:'20px'
-      },
-      colors: {
-        vuejs: "#49e659",
-      },
-    },
-  },
+  theme: {},
   plugins: [],
 };
